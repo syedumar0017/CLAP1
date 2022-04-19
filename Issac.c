4ac5d2661c84bca9944de39ba0f44401ccc97a1c
syedumar0017 <103935527+syedumar0017@users.noreply.github.com>
2022-04-19 15:06:57 +0530

    Create Issac.c

---
+++ Issac.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+
+int main() {
+
+    float tractLand;
+
+    scanf("%f", &tractLand);
+
+    float acre = tractLand / 43560;
+
+    printf("%.2f sq.ft is equal to %.2f acres", tractLand, acre);
+
+    return 0;
+
+}
